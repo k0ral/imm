4ef9a531505edfdbb47e2d6e5fc81b38c59f1169
koral <koral att mailoo dott org>
2012-08-11 11:13:20 +0200

    Updated example configuration file, including some comments.

--- example/imm.hs
+++ example/imm.hs
@@ -10,13 +10,17 @@ import System.Directory
 import System.Environment.XDG.BaseDir
 -- }}}
 
+-- The 'main' function must call 'imm' with a feed list
 main :: IO ()
-main = imm myConf
+main = imm myFeeds
 
+-- Feeds are a list of tuples (custom settings, uri)
+myFeeds :: FeedList
+myFeeds = zip (repeat mySettings) myUris
 
-myConf :: FeedList
-myConf = zip (repeat mySettings) feeds
+-- Custom settings transform default settings (cf 'Imm.Types.Settings')
+mySettings :: CustomSettings
+mySettings = id
 
-
- 
-feeds =  ["http://planet.haskell.org/rss20.xml","http://xkcd.com/rss.xml" ]
+-- Uris are bare String and will be parsed inside imm
+myUris =  ["http://planet.haskell.org/rss20.xml", "http://xkcd.com/rss.xml"]
