989c9405cb7fa1397c84061c6e92f7978e903878
koral <koral@mailoo.org>
2016-11-20 16:02:01 +0100

    Better filepath sanitization

--- src/lib/Imm/Hooks/WriteFile.hs
+++ src/lib/Imm/Hooks/WriteFile.hs
@@ -11,6 +11,8 @@ import           Imm.Hooks
 import           Imm.Prelude
 import           Imm.Pretty
 
+import           Control.Arrow
+
 import           Data.Monoid.Textual           hiding (map)
 import qualified Data.Text.Lazy                as Text
 import           Data.Time
@@ -19,9 +21,10 @@ import           System.Directory              (createDirectoryIfMissing)
 import           System.FilePath
 
 import           Text.Atom.Types
-import qualified Text.Blaze                    as Blaze
 import           Text.Blaze.Html.Renderer.Text
-import           Text.Blaze.Html5              as H hiding (map)
+import           Text.Blaze.Html5              (Html, docTypeHtml,
+                                                preEscapedToHtml, (!))
+import qualified Text.Blaze.Html5              as H hiding (map)
 import           Text.Blaze.Html5.Attributes   as H (charset, href)
 import           Text.RSS.Types
 
@@ -57,9 +60,11 @@ defaultSettings root = WriteFileSettings $ \feed element -> FileInfo
 defaultFilePath :: FilePath -> Feed -> FeedElement -> FilePath
 defaultFilePath root feed element = makeValid $ root </> feedTitle </> fileName <.> "html" where
   date = maybe "" (formatTime defaultTimeLocale "%F-") $ getDate element
-  fileName = date <> convertText (sanitizePath $ getTitle element)
-  feedTitle = convertText $ sanitizePath $ getFeedTitle feed
-  sanitizePath = intercalate "-" . split isPathSeparator
+  fileName = date <> sanitize (convertText $ getTitle element)
+  feedTitle = sanitize $ convertText $ getFeedTitle feed
+  sanitize = replaceIf isPathSeparator '-' >>> replace '.' '_' >>> replace '?' '_'
+  replace a b = replaceIf (== a) b
+  replaceIf f b = map (\c -> if f c then b else c)
 
 -- | Generate an HTML page, with a title, a header and an article that contains the feed element
 defaultFileContent :: Feed -> FeedElement -> ByteString
@@ -67,9 +72,9 @@ defaultFileContent feed element = encodeUtf8 $ Text.toStrict $ renderHtml $ docT
   H.head $ do
     H.meta ! H.charset "utf-8"
     H.title $ convertText $ getFeedTitle feed <> " | " <> getTitle element
-  body $ do
+  H.body $ do
     H.h1 $ convertText $ getFeedTitle feed
-    article $ do
+    H.article $ do
       defaultHeader feed element
       defaultBody feed element
 
@@ -78,29 +83,29 @@ defaultFileContent feed element = encodeUtf8 $ Text.toStrict $ renderHtml $ docT
 
 -- | Generate an HTML @<header>@ for a given feed element
 defaultHeader :: Feed -> FeedElement -> Html
-defaultHeader _ element@(RssElement item) = header $ do
+defaultHeader _ element@(RssElement item) = H.header $ do
   H.h2 $ maybe id (\uri -> H.a ! H.href uri) link $ convertText $ getTitle element
-  unless (null author) $ address $ "Published by " >> convertText author
-  forM_ (itemPubDate item) $ \date -> p $ " on " >> time (convertDoc $ prettyTime date)
+  unless (null author) $ H.address $ "Published by " >> convertText author
+  forM_ (itemPubDate item) $ \date -> H.p $ " on " >> H.time (convertDoc $ prettyTime date)
   where link = withRssURI (convertDoc . prettyURI) <$> itemLink item
         author = itemAuthor item
-defaultHeader _ element@(AtomElement entry) = header $ do
+defaultHeader _ element@(AtomElement entry) = H.header $ do
   H.h2 $ convertText $ getTitle element
-  address $ do
+  H.address $ do
     "Published by "
     forM_ (entryAuthors entry) $ \author -> do
       convertDoc $ prettyPerson author
       ", "
-  p $ "on " >> time (convertDoc $ prettyTime $ entryUpdated entry)
+  H.p $ "on " >> H.time (convertDoc $ prettyTime $ entryUpdated entry)
 
 -- | Generate the HTML content for a given feed element
 defaultBody :: Feed -> FeedElement -> Html
-defaultBody _ (RssElement item) = p $ preEscapedToHtml $ itemDescription item
+defaultBody _ (RssElement item) = H.p $ preEscapedToHtml $ itemDescription item
 defaultBody _ (AtomElement entry) = do
-  unless (null links) $ p $ do
+  unless (null links) $ H.p $ do
     "Related links:"
     H.ul $ forM_ links $ \uri -> H.li (H.a ! H.href (convertAtomURI uri) $ convertAtomURI uri)
-  p $ preEscapedToHtml $ fromMaybe "<empty>" $ content <|> summary
+  H.p $ preEscapedToHtml $ fromMaybe "<empty>" $ content <|> summary
   where links   = map linkHref $ entryLinks entry
         content = show . prettyAtomContent <$> entryContent entry :: Maybe Text
         summary = show . prettyAtomText <$> entrySummary entry :: Maybe Text
