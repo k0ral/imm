8f9675639c7264b3569ed47fdf67ef47bcac81d6
koral <mail@cmoreau.info>
2020-10-12 15:15:33 +0200

    Add `headFail` and `headThrow` functions to Prelude module

--- src/Prelude.hs
+++ src/Prelude.hs
@@ -1,10 +1,19 @@
-module Prelude (for, io, module Relude, module Relude.Extra.Map) where
+module Prelude (for, io, headFail, headThrow, module Relude, module Relude.Extra.Map) where
 
 import           Relude           hiding (Handle, appendFile, force, readFile, stdout, writeFile)
 import           Relude.Extra.Map (lookup)
+import           Control.Exception.Safe
 
 io :: MonadIO m => IO a -> m a
 io = liftIO
 
 for :: [a] -> (a -> b) -> [b]
 for = flip map
+
+headThrow :: MonadThrow m => Exception e => e -> [a] -> m a
+headThrow _ (a:_) = return a
+headThrow e _ = throwM e
+
+headFail :: MonadFail m => String -> [a] -> m a
+headFail _ (a:_) = return a
+headFail e _ = fail e
