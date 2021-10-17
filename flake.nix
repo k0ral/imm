f2f68e52c2d486bd42e8e2938b428c02dd6f1d60
koral <mail@cmoreau.info>
2021-10-17 18:31:22 +0200

    Upgrade nixpkgs dependency

--- flake.nix
+++ flake.nix
@@ -2,15 +2,13 @@
   description = "imm flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/8e4fe32876ca15e3d5eb3ecd3ca0b224417f5f17";
+    nixpkgs.url = "github:NixOS/nixpkgs/31ffc50c571e6683e9ecc9dbcbd4a8e9914b4497";
     flake-utils.url = "github:numtide/flake-utils/2ebf2558e5bf978c7fb8ea927dfaed8fefab2e28";
     beam.url = "github:haskell-beam/beam/efd464b079755a781c2bb7a2fc030d6c141bbb8a";
     beam.flake = false;
-    rss-conduit.url = "github:k0ral/rss-conduit/c1fec73d715fd1c9a95a155e87ba469887b8e543";
-    rss-conduit.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, beam, rss-conduit }:
+  outputs = { self, nixpkgs, flake-utils, beam }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -33,7 +31,6 @@
               beam-core = hlib.doJailbreak (hself.callCabal2nix "beam-core" "${beam}/beam-core" { });
               beam-migrate = hlib.doJailbreak (hself.callCabal2nix "beam-core" "${beam}/beam-migrate" { });
               beam-sqlite = hlib.doJailbreak (hself.callCabal2nix "beam-core" "${beam}/beam-sqlite" { });
-              rss-conduit = hself.callCabal2nix "rss-conduit" rss-conduit { };
             };
         };
 
